8895d99417b9ddfda5d9b2ea71474bf5a6b7b3c5
Parsa <mehdipourparsa@gmail.com>
2024-10-25 18:42:29 -0400

    feat: Add interfaces

---
+++ DemoLibrary/Interfaces/IDiscountStrategy.cs
@@ -0,0 +1,13 @@
+﻿namespace DemoLibrary.Interfaces;
+
+/// <summary>
+/// The Strategy interface declares operations common to all supported
+/// versions of some algorithm.
+///
+/// The Context uses this interface to call the algorithm defined by Concrete
+/// Strategies.
+/// </summary>
+public interface IDiscountStrategy
+{
+    decimal CalculateDiscount(decimal orderTotal, Customer customer);
+}
