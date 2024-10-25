2c96bdf85dd9535e30e5e71a28ea5db0c8f5b454
Parsa <mehdipourparsa@gmail.com>
2024-10-25 18:42:58 -0400

    feat: Add customer

---
+++ DemoLibrary/Customer.cs
@@ -0,0 +1,20 @@
+﻿namespace DemoLibrary;
+
+/// <summary>
+/// Represents a customer with relevant properties for discount calculation
+/// </summary>
+public class Customer
+{
+    public string Name { get; set; }
+    public DateTime JoinDate { get; set; }
+    public int LoyaltyPoints { get; set; }
+    public bool IsVipMember { get; set; }
+
+    public Customer(string name, DateTime joinDate, int loyaltyPoints, bool isVipMember)
+    {
+        Name = name;
+        JoinDate = joinDate;
+        LoyaltyPoints = loyaltyPoints;
+        IsVipMember = isVipMember;
+    }
+}
